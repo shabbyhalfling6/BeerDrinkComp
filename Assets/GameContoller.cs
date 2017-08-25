f661a84cacd8d2cf97ba04d934a813763d32b8df
SAE\bjohnston3 <bradycjohnston@hotmail.com>
2017-08-25 19:18:42 +1000

    Initial Commit

---
+++ Assets/GameContoller.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameContoller : MonoBehaviour {
+
+
+    public bool gameOver = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    void GameOver()
+    {
+        //set game over screen to true and pause the game
+    }
+
+    void MainMenu()
+    {
+        //set the main menu to true and pause the game
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public bool player1 = true;
+
+    public bool drink = false;
+
+    public int drunkness = 0;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+        PlayerInput();
+	}
+
+    void PlayerInput()
+    {
+        if (player1)
+        {
+            if (Input.GetKeyDown(KeyCode.A) && !drink)
+            {
+                drink = true;
+                drunkness++;
+                DrinkADrink();
+            }
+            else if (Input.GetKeyDown(KeyCode.S) && drink)
+            {
+                drink = false;
+                drunkness++;
+                DrinkADrink();
+            }
+        }
+        else
+        {
+            if(Input.GetKeyDown(KeyCode.K) && !drink)
+            {
+                drink = true;
+                drunkness++;
+                DrinkADrink();
+            }
+            else if(Input.GetKeyDown(KeyCode.L) && drink)
+            {
+                drink = false;
+                drunkness++;
+                DrinkADrink();
+            }
+        }
+    }
+
+    void DrinkADrink()
+    {
+        //make the sprite change here
+    }
+}
